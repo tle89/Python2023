b07467d63747e84703bd7c24839cf444937c1d36
Thinh Le <thinh191989@gmail.com>
2023-08-18 18:43:42 -0500

    Created examples.py to demonstrate Python For Loop

---
+++ Basics/For Loop/examples.py
@@ -0,0 +1,16 @@
+# word = "hello"
+# for letter in word:
+#     print(letter)
+
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# for i in range(5):
+#     print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    if fruit == "banana":
+        break
+    print(fruit)  
