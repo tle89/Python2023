a3a2dd01b3db8cfa508ca2e0f44c9eb5387c20e0
Thinh Le <thinh191989@gmail.com>
2023-08-18 23:41:16 -0500

    Write a program to print numbers from 1 to 10 using a for loop

---
+++ Basics/For Loop/for_1.py
@@ -0,0 +1,27 @@
+# Write a program to print numbers from 1 to 10 using a for loop
+
+# Solution:
+for i in range(1,11):
+    print(i)
+# Output: will be printed in the console on the new line
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# Explanation:
+# Alright! Imagine you have a box of toy blocks. Each block has a number on it. The `range()` function is like a magical toy machine that helps you line up these blocks in a specific order.
+
+# Let's say you want blocks numbered from 1 to 5. You'd use the magical toy machine like this: `range(1, 6)`. And guess what? The machine will show you blocks: 1, 2, 3, 4, 5. The first number you tell the machine is where to start, and the second number is where to stop (but it won't include that last number, which is why we say 6 to get up to 5).
+
+# Now, what if you want every other block? Let's say you want 1, then skip to 3, then skip to 5. You can tell the machine: `range(1, 6, 2)`. That last number, 2, tells the machine to skip one block in between.
+
+# So, think of `range()` as a cool toy machine that helps you set up your blocks in the order you want!
+
+    
